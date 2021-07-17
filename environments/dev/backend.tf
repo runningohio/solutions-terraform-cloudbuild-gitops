c21ac8dbda62519d9a4061aa7f8997b7c35c113f
mike <mike@runningohio.com>
2021-07-17 15:31:13 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "terraform-build-320019-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="terraform-build-320019"
